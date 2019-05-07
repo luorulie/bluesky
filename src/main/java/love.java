79860db5a2fbe2642bf5187ee223e69740b9115a
abc <abc@qq.com>
2019-05-07 15:16:27 +0800

    新添加

--- src/main/java/love.java
+++ src/main/java/love.java
@@ -1,5 +1,7 @@
-publi class love(){
+public class love{
 	public static void main(String arg[]){
+		System.out.println("first hello world");
+		System.out.println("first hello world");
 		System.out.println("first hello world");
 	}
 }
